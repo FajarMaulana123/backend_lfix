d2130f1a7405042508657e0f142cf606137ca028
unknown <id.fajarmaulana@gmail.com>
2020-02-29 19:49:28 +0700

    udah cek

--- app/M_Barang.php
+++ app/M_Barang.php
@@ -10,7 +10,7 @@ class M_Barang extends Model
     public $timestamps = false;
 
     protected $table = 'barang';
-    protected $primaryKey = 'kode_barang';
+    // protected $primaryKey = 'kode_barang';
     protected $fillable = ['kode_barang', 'jenis_barang', 'icon'];
 
 }
